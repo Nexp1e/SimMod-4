f92d74a6b6778fc6016e46681f5c1644b843a1c9
Nexp1e <nexp1e@stud.tsu.ru>
2022-03-15 04:49:42 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace SimMod_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.table = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timeText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // table
+            // 
+            this.table.AllowUserToAddRows = false;
+            this.table.AllowUserToDeleteRows = false;
+            this.table.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.Location = new System.Drawing.Point(12, 12);
+            this.table.Name = "table";
+            this.table.Size = new System.Drawing.Size(552, 553);
+            this.table.TabIndex = 0;
+            this.table.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.table_CellClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(636, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 68);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start/Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timeText
+            // 
+            this.timeText.Location = new System.Drawing.Point(676, 86);
+            this.timeText.Name = "timeText";
+            this.timeText.Size = new System.Drawing.Size(100, 20);
+            this.timeText.TabIndex = 2;
+            this.timeText.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(635, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Time:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 577);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.timeText);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.table);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox timeText;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimMod_4
+{
+    public partial class Form1 : Form
+    {
+        int n = 8;
+        public List<List<int>> cells = new List<List<int>>();
+        bool isInit = false;
+        int time = 0;
+
+        void InitCells()
+        {
+            cells.Clear();
+            for (int i = 0; i < n; i++)
+            {
+                cells.Add(new List<int>());
+                for (int j = 0; j < n; j++)
+                {
+                    cells[i].Add(0);
+                }
+            }
+        }
+
+        void InitDataGrid()
+        {
+            table.Columns.Clear();
+            table.Rows.Clear();
+
+            for (int i = 0; i < n; i++)
+            {
+                table.Columns.Add(new DataGridViewColumn() { CellTemplate = new DataGridViewTextBoxCell() });
+            }
+            for (int i = 0; i < n; i++)
+            {
+                table.Rows.Add();
+                for (int j = 0; j < n; j++)
+                {
+                    table.Rows[i].Cells[j].Value = 0;
+                    cells[i][j] = 0;
+                }
+            }
+        }
+
+        void Init()
+        {
+            if (!isInit)
+            {
+                InitCells();
+                InitDataGrid();
+                time = 0;
+                timeText.Text = time.ToString();
+                isInit = true;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Enabled = false;
+                isInit = false;
+            }
+            else
+            {
+                Init();
+                timer1.Enabled = true;
+            }
+        }
+
+        int GetNeighbourCellsValue(List<List<int>> prevCells, int x, int y)
+        {
+            int sum = 0;
+
+            for (int i = Math.Max(x - 1, 0); i <= Math.Min(x + 1, n - 1); i++)
+            {
+                for (int j = Math.Max(y - 1, 0); j <= Math.Min(y + 1, n - 1); j++)
+                {
+                    if (!(i == x && j == y))
+                    {
+                        sum += prevCells[i][j];
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            List<List<int>> prevCells = new List<List<int>>(cells);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (prevCells[i][j] == 0)
+                    {
+                        if (GetNeighbourCellsValue(prevCells, i, j) == 3)
+                        {
+                            cells[i][j] = 1;
+                            SetCellColor(1, i, j);
+                        }
+                    }
+                    else
+                    {
+                        switch (GetNeighbourCellsValue(prevCells, i, j))
+                        {
+                            case 2:
+                            case 3:
+                                cells[i][j] = prevCells[i][j];
+                                SetCellColor(cells[i][j], i, j);
+                                break;
+                            default:
+                                cells[i][j] = 0;
+                                SetCellColor(0, i, j);
+                                break;
+                        }
+                    }
+                }
+            }
+            time++;
+            timeText.Text = time.ToString();
+        }
+
+        void SetCellColor(int value, int x, int y)
+        {
+            if (value == 1)
+            {
+                table.Rows[y].Cells[x].Style.BackColor = Color.Red;
+            }
+            else
+            {
+                table.Rows[y].Cells[x].Style.BackColor = table.DefaultCellStyle.BackColor;
+            }
+        }
+
+        private void table_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            table.CurrentCell.Selected = false;
+            DataGridViewCellStyle CellStyle = new DataGridViewCellStyle();
+            if (cells[e.ColumnIndex][e.RowIndex] == 1)
+            {
+                cells[e.ColumnIndex][e.RowIndex] = 0;
+                CellStyle.BackColor = table.DefaultCellStyle.BackColor;
+            }
+            else
+            {
+                cells[e.ColumnIndex][e.RowIndex] = 1;
+                CellStyle.BackColor = Color.Red;
+            }
+            table.Rows[e.RowIndex].Cells[e.ColumnIndex].Style = CellStyle;
+
+        }
+    }
+}
